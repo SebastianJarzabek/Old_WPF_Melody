b3dea6e31a7c8489d679d887b82f1dfa9dd4bda6
Sebastian Jarząbek <sebastian.jarzabek@outlook.com>
2019-10-06 22:43:54 +0200

    Working on Main view.

---
+++ Melody/View/Main.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Melody.View
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml.
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+    }
+  }
+}
