41e403ee7750223e4f3fa6a07ed6ed9e8fa674de
Sebastian Jarząbek <sebastian.jarzabek@outlook.com>
2019-10-06 19:53:55 +0200

    Add Log in view.

---
+++ Melody/View/LogIn.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Melody
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml.
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
